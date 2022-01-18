239a02b12e3617093e5b32d00dfb17bce99be5f6
Andrey Esman <andreyesman.md@gmil.com>
2022-01-18 14:57:40 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.add("active");
+  nav.classList.toggle("active");
+});
