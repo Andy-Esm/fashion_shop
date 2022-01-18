7b4e784de014ab03cffcd5e9a6f50941ef1f791c
Andrey Esman <andreyesman.md@gmil.com>
2022-01-18 09:47:20 +0300

    Выполнена частичная адаптация под планшеты и мобильные устройства

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById("toggle");
-const nav = document.getElementById("nav");
-
-toggle.addEventListener("click", () => {
-  toggle.classList.add("active");
-  nav.classList.toggle("active");
-});
