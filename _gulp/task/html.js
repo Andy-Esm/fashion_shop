ae0723009dd5ad7efc13ff2cfa57c0a4f3caa4a1
Andrey Esman <andreyesman.md@gmil.com>
2022-01-21 18:48:52 +0300

    Доделан адаптив главной страницы для планшетов и мобилок + responsive, заменены px на rem

--- _gulp/task/html.js
+++ _gulp/task/html.js
@@ -4,5 +4,5 @@ export const html = () => {
 	return app.gulp.src(app.path.src.html)
 	.pipe(fileInclude())
 	.pipe(app.gulp.dest(app.path.build.html))
-	.pipe(app.plugins.browserSync.stream());
+	.pipe(app.plugins.browserSync.stream())
 }
